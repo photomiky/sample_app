fda2505bbb6244febf04509b34561dd1500305f7
Michael Rabin <photomiky@gmail.com>
2011-05-22 12:09:12 +0300

    User model, password, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
     @title = "Sign Up"
   end
 
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+end
+
+module UsersHelper
+  def gravatar_for(user, options = { :size => 50 })
+    gravatar_image_tag(user.email.downcase, :alt => user.name, 
+                                            :class => 'gravatar',
+                                            :gravatar => options)
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,37 @@ describe UsersController do
 
   render_views
   
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should be successfull" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+    it "should find the right user" do
+      get :show, :id => @user 
+      assigns(:user).should == @user
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+    
+    it "should have the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content=> @user.name)
+    end
+    
+    it "should have an avatar image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class=> "gravatar")
+    end
+  end
+  
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
@@ -15,6 +46,8 @@ describe UsersController do
     end
     
   end
+  
+  
   
   
 
